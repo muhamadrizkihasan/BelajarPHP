5ba811539a95472905445aadd50a3e5869a8345d
Muhamad Rizki Hasan <muhamadrizkihasan@smkwikrama.sch.id>
2023-09-12 09:51:11 +0700

    Menambahkan folder Syntaks

---
+++ 2_Syntaks/index.php
@@ -0,0 +1,57 @@
+<?php
+    /*
+    Pertemuan 2 - PHP Dasar
+    1.Sintaks PHP
+        -Standar Output
+        -echo, print
+        -print_r
+        -var_dump
+
+    2.Penulisan Sintaks PHP
+        1. PHP Di Dalam HTMl
+        2. HTML Di Dalam PHP
+
+    3.Variabel Dan Tipe Data
+        Variabel Tidak Boleh Diawali Dengan Angka,Tapi Boleh Mengandung Angka
+        $nama = "Muhamad Rizki Hasan";
+        echo 'Halo, Nama Saya $nama'; -> Jika Menggunakan Kutip Satu Maka Akan Eror,
+        Jadi, Harus Mengunakan Kutip Dua.
+
+    4.Operator Aritmatika
+        +, -, *, /, &
+        $x = 10;
+        $y = 20;
+        echo $x * $y;
+
+    5.Penggabung String/Concatenation/concat
+        .
+        $nama_depan = "Rizki";
+        $nama_belakan = "Hasan";
+        echo $nama_depan . " " . $nama_belakang;
+
+    6.Assignment
+        =, +=, -=, *=, /=, %=, .=
+        $x = 1;
+        $x -= 2;
+        echo $x;
+        $nama $x;
+        $nama = "Rizki";
+        $nama .= " ";
+        $nama .= "Hasan";
+        echo $nama;
+
+    7.Perbandingan
+        <, >, <=, >=, ==, !=
+        var_dump(1 == "1");
+        
+    8.Identitas
+        ===, !==
+        var_dump(1 === "1");
+
+    9.Logika
+        &&, ||, !
+        $x = 10;
+        $x = 30;
+        var_dump($x < 20 || $x % 2 == 0);
+    */
+?>
